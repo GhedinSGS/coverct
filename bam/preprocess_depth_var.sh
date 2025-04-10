137ec47b9120556e79164d8a412cbf93efcbe40d
Tung H. Nguyen <111020529+tungwinxp@users.noreply.github.com>
2025-04-10 10:13:34 +0100

    Create preprocess_depth_var.sh

---
+++ bam/preprocess_depth_var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Tung Nguyen
+module load samtools ivar
+
+# samtools mpileup corresponds to getting the depth per nucleotide position on the genome
+
+
+# ivar corresponds to intrahost variant calling at each position
